18e56dc53f0bda068be966e8cc42100345b3e031
Phillip Webb <phil.webb@broadcom.com>
2025-08-22 18:30:28 -0700

    Add `isdb-web` sample application
    Add a new sample Spring MVC application that calls the rest API
    and displays HTML from mustache templates.
    
    The application doesn't currently work because the HTTP service
    client is not associated with any URL.

---
+++ isdb-web/src/main/java/com/example/isdbweb/StuffieHttpClient.java
@@ -0,0 +1,23 @@
+package com.example.isdbweb;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+
+public interface StuffieHttpClient {
+
+	@GetExchange("/stuffies")
+	List<Stuffie> all();
+
+	@GetExchange("/stuffie/{id}/detail")
+	Stuffie detail(@PathVariable int id);
+
+	@GetExchange(url = "/stuffie/{id}/for")
+	List<String> owners(@PathVariable int id);
+
+	@GetExchange("/stuffie/{id}/pic")
+	InputStream picture(@PathVariable int id);
+
+}
