334830d98c18747f13722f3509e4ca143753c76c
Phillip Webb <phil.webb@broadcom.com>
2025-08-22 18:31:04 -0700

    Use resiliency features to protect against timeouts
    Use `@Retryable` on the service method to automatically retry
    failed calls. The `@EnableResilientMethods` has also been added
    to enable support.

--- isdb-web/src/main/java/com/example/isdbweb/IsdbWebApplication.java
+++ isdb-web/src/main/java/com/example/isdbweb/IsdbWebApplication.java
@@ -2,8 +2,10 @@ package com.example.isdbweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @SpringBootApplication
+@EnableResilientMethods
 public class IsdbWebApplication {
 
 	public static void main(String[] args) {
